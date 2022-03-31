f78ea34c900f4e1bc4eb626b8faf814148a28e7b
Heinrick Senna <marcelohz198@hotmail.com>
2022-03-31 02:20:33 -0300

    Creating Script Prod

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.use('/', (req, res) => { res.send('Isto é um teste') })
+app.use('/', (req, res) => { res.send('Alteração Simples') })
 app.listen(80, console.log('Servidor Rodando'))
