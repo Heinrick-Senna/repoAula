aa58e0c89dd06fa8e54c2d7e2358014e278f1b98
Heinrick Senna <marcelohz198@hotmail.com>
2022-03-31 02:33:25 -0300

    Alterando Texto Exibido

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.use('/', (req, res) => { res.send('Alteração Simples') })
+app.use('/', (req, res) => { res.send('Marcelo Senna') })
 app.listen(80, console.log('Servidor Rodando'))
